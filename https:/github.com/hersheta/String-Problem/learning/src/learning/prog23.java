d43288a584438a5c6d35e4802a652f3f65a150fa
harshitasrivastava <harshitasrivastava@localhost>
2023-04-17 13:05:43 +0530

    committing string practice

---
+++ https:/github.com/hersheta/String-Problem/learning/src/learning/prog23.java
@@ -0,0 +1,24 @@
+package learning;
+
+import java.util.Arrays;
+
+public class prog23 {
+			public static void main(String args[]) {
+ String s="I am Harshita";	
+ char[]arr=s.toCharArray();
+			{
+				for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+
+				
+		}
+
+	}
+	}
+}
+
+	//join string of array to arrya
+	//converting string array to string
+	//convert arraytoarraylist
+
+//if i want to remove the whitespaces
